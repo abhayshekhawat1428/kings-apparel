b6d4473c0dd7a02b3b8aeca07ad797378597f1cd
Abhay Shekhawat <shekhawat.abhay94@gmail.com>
2020-07-12 18:51:14 +0530

    Added header to the project and links to home and shop in the header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {HomePage} from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
@@ -13,6 +14,8 @@ const HatsPage = () =>(
 function App() {
   return (
     <div>
+      <Header/>{/* We keep this header outside the switch which 
+      makes it always present despite what route we are on */}
       <Switch>
         <Route exact path='/' component ={HomePage}/>
         <Route path='/shop' component ={ShopPage}/>  
